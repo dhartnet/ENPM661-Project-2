397885d1fe79cb6f8445bfcad2d8e8b72f0a2dbd
dhartnet <dhartnet@umd.edu>
2024-03-10 14:32:28 -0400

    Video and Final code
    video from (10, 250) to (1150, 250) and finalized code

--- Project2.py
+++ Project2.py
@@ -1,9 +1,10 @@
 import numpy as np
 from queue import PriorityQueue
 import time
-import matplotlib.pyplot as plt
 import cv2
 import os
+
+# Input start and goal coordinates 
 
 Startx = int(input('Starting X Coordinate:  '))
 print('')
@@ -22,6 +23,7 @@ def Dijkstra(Startx, Starty, Goalx, Goaly):
   # Dictionary = Closed List
   closed = {}
 
+  # A set of nodes in open list to check going forward
   openCheck = set()
 
   # Start Coordinates
@@ -48,6 +50,7 @@ def Dijkstra(Startx, Starty, Goalx, Goaly):
   print('#-----Calculating Path To Goal Coordinate-----#')
   print('')
 
+  # Initialize node index
   nodeindex = 0
 
   # Begin BFS loop to explore nodal tree (aka run search while there are nodes to search)
@@ -87,8 +90,12 @@ def Dijkstra(Startx, Starty, Goalx, Goaly):
         # Check to see if node is in free space, if it is then add it to the open list queue
         if inObstacle(maybeNode) == False:
 
+          # Check if node is already in the open list, if it is then skip node
+          # It is not possible to reach a node with more efficiency if it has already been reached using the shortest path
+          # when using Dijkstra method with uniform costs
           if maybeNode not in openCheck:
 
+            # If not has not been visited, add it to the priority queue and the openCheck set
             nodeindex = nodeindex + 1
             ope.put((round((parentcost+c), 2), nodeindex, parentnodeindex, maybeNode))
             openCheck.add(maybeNode)
@@ -180,6 +187,7 @@ def inObstacle(maybeNode):
 # Back Track algorithm to find optimized path to goal
 def BackTrack(closed, Startx, Starty, Goalx, Goaly):
 
+  # Initialize arrays containing node coordinates for plotting purposes
   pathx = []
 
   pathy = []
@@ -187,6 +195,7 @@ def BackTrack(closed, Startx, Starty, Goalx, Goaly):
   goalNode = (Goalx, Goaly)
   startNode = (Startx, Starty)
 
+  # Add goal node to back tracking list of nodes
   goalInfo = closed.get(goalNode)
 
   nodeState = goalInfo[3]
@@ -195,6 +204,8 @@ def BackTrack(closed, Startx, Starty, Goalx, Goaly):
   pathx.append(nodeState[0])
   pathy.append(nodeState[1])
 
+  # Check nodes to find parent nodes starting from goal node and working back to the start node
+  # Once the parent node is found, make that the new active node and add its coordinates to the path
   while nodeState != startNode:
 
     for key in closed:
@@ -210,6 +221,7 @@ def BackTrack(closed, Startx, Starty, Goalx, Goaly):
         nodeState = coord
         break
 
+  # Reverse the arrays so I can plot from start node to goal node
   pathx.reverse()
   pathy.reverse()
 
@@ -218,12 +230,17 @@ def BackTrack(closed, Startx, Starty, Goalx, Goaly):
 
 ###---------------------------------------###
 
+# Start clock
 start = time.time()
 
+# Run Dijkstra Algorithm to explore nodes until the goal node is reached
+# Return the closed list
 closed = Dijkstra(Startx, Starty, Goalx, Goaly)
 
+# Run the back track algorithm and return x and y coordinates of path
 [pathx, pathy] = BackTrack(closed, Startx, Starty, Goalx, Goaly)
 
+# Stop clock and prin time
 end = time.time()
 
 print('')
@@ -242,7 +259,9 @@ red = (0, 0, 255)
 orange = (0, 105, 30)
 yellow = (255,0,205)
 white = (255, 255, 255)
+purple = (128, 0, 128)
 
+# Make white canvas
 map = np.ones((500,1200,3), dtype=np.uint8) # blank map
 map = 255*map # make it white
 
@@ -253,12 +272,12 @@ cv2.rectangle(map, (100,0), (175,400), green, -1)
 # Second Obstacle
 cv2.rectangle(map, (275,100), (350,500), green, -1)
 
-# Third Obstacle
+# Third Obstacle - Hexagon
 hexpts = np.array([[520, 325], [650, 400], [780, 325], [780, 175], [650, 100], [520, 175]])
 hexpts = hexpts.reshape(-1,1,2)
 cv2.fillPoly(map, [hexpts], green)
 
-# Fourth Obstacle
+# Fourth Obstacle - Concave Obstacle
 concavepts = np.array([[900, 450], [1100, 450], [1100, 50], [900, 50], [900, 125], [1020, 125], [1020, 375], [900, 375]])
 concavepts = concavepts.reshape(-1,1,2)
 cv2.fillPoly(map, [concavepts], green)
@@ -271,12 +290,12 @@ cv2.rectangle(map, (95,5), (180,405), red, 3)
 # Second Obstacle
 cv2.rectangle(map, (270,500), (355,95), red, 3)
 
-# Third Obstacle
+# Third Obstacle - Hexagon
 hexpts = np.array([[515, 330], [650, 405], [785, 330], [785, 170], [650, 95], [515, 170]])
 hexpts = hexpts.reshape(-1,1,2)
 cv2.polylines(map, [hexpts], isClosed=True, thickness=3, color=red)
 
-# Fourth Obstacle
+# Fourth Obstacle - Concave Obstacle
 concavepts = np.array([[895, 455], [1105, 455], [1105, 45], [895, 45], [895, 130], [1015, 130], [1015, 370], [895, 370]])
 concavepts = concavepts.reshape(-1,1,2)
 cv2.polylines(map, [concavepts], isClosed=True, thickness=3, color=red)
@@ -287,33 +306,51 @@ cv2.rectangle(map, (0,0), (1200,500), yellow, 3)
 # map border + 5 pixel buffer
 cv2.rectangle(map, (5,495), (1195, 5), (0,0,0), 1)
 
-# empty image array to store frames
+# Plot Start Node
+cv2.drawMarker(map, (Startx,500-Starty), color = blue, thickness=3, markerType= cv2.MARKER_SQUARE, line_type=cv2.LINE_AA, markerSize=3)
 
+# Plot Goal Node
+cv2.drawMarker(map, (Goalx,500-Goaly), color = purple, thickness=3, markerType= cv2.MARKER_STAR, line_type=cv2.LINE_AA, markerSize=3)
+
+# empty image array to store frames
 image_array = []
 
 # Define video file
-
 out = cv2.VideoWriter( "dhartnet_Project2.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 20, (1200, 500)) 
 
-# loop through closed coordinates and plot one by one on the map
+# Initialize value to periodically plot nodes to shorten video
+f = 0
+
+# loop through closed coordinates and plot one by one on the map then add frames to the video
 for key in closed:
   information = closed.get(key)
   coord = information[3]
   x = coord[0]
   y = 500-coord[1]
   cv2.drawMarker(map, (x,y), color = [0, 165, 255], thickness=2, markerType= cv2.MARKER_SQUARE, line_type=cv2.LINE_AA, markerSize=1)
-  out.write(map)
-  if cv2.waitKey(1) & 0xFF == ord('s'): 
-    break
+  if f % 500 == 0:
+    # Plot Start Node
+    cv2.drawMarker(map, (Startx,500-Starty), color = blue, thickness=3, markerType= cv2.MARKER_SQUARE, line_type=cv2.LINE_AA, markerSize=3)
+    # Plot Goal Node
+    cv2.drawMarker(map, (Goalx,500-Goaly), color = purple, thickness=3, markerType= cv2.MARKER_SQUARE, line_type=cv2.LINE_AA, markerSize=3)
+    out.write(map)
+    if cv2.waitKey(1) & 0xFF == ord('s'): 
+      break
+  f = f + 1
 
-# loop through optimized Path coordinates and plot one by one on the map
+# Initialize value to plot path in chunks to shorten video
+g = 0
 
+# loop through optimized Path coordinates and plot one by one on the map and then add to video
 for x1,y1 in zip(pathx,pathy):
   cv2.drawMarker(map, (x1,500-y1), color = [0, 0, 0], thickness=2, markerType= cv2.MARKER_SQUARE, line_type=cv2.LINE_AA, markerSize=1)
-  out.write(map)
-  if cv2.waitKey(1) & 0xFF == ord('s'): 
-    break
+  if g % 5 == 0:
+    out.write(map)
+    if cv2.waitKey(1) & 0xFF == ord('s'): 
+      break
+  g = g +1
 
+# release video
 cv2.destroyAllWindows() 
 out.release()
 
